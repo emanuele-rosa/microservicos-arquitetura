1e826fe52d40c0996f2bf5c1ba6f356dc204928b
emanuele-rosa <emanuele.rosa@startse.com>
2025-01-16 17:08:59 -0300

    feat: create enums

---
+++ src/enums/OrderStatus.js
@@ -0,0 +1,9 @@
+const OrderStatus = {
+  CREATED: "CREATED",
+  PENDING_PAYMENT: "PENDING_PAYMENT",
+  PAID: "PAID",
+  CANCELLED: "CANCELLED",
+  FAILED: "FAILED",
+};
+
+export default OrderStatus;
